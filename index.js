e489f1c52545326f451ab9c456aeb1c226047d09
Keenan Weise <kcweise@gmail.com>
2024-02-20 15:52:37 -0600

    First Class functions lab

---
+++ index.js
@@ -0,0 +1,17 @@
+const chai = "chai";
+const spies = "chai-spies";
+   
+function receivesAFunction(callback){
+
+    callback()
+    return 
+}
+receivesAFunction()
+
+function returnsANamedFunction(){
+    return function willy(){}
+}
+
+function returnsAnAnonymousFunction(){
+    return ()=>{}
+}
